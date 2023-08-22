064dc0897a60517972236a0c79478c33c1b4e388
rashedulalam362 <rashedulalam362@gmail.com>
2023-08-22 13:12:42 +0600

    prosessing

--- app/page.js
+++ app/page.js
@@ -1,7 +1,37 @@
-import Image from 'next/image'
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://basic-blog.teamrabbil.com/api/post-newest')
+  const data= await res.json()
+ 
+  if (!res.ok) {
+   
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return data;
+}
+
+export default async function Home() {
+  const data =await getData()
+
   return (
-   <h3>THIS IS HOME PAGE </h3>
+    <div className="container mx-auto   );
+    gap: 6px;">
+        <div className="grid grid-cols gap-6 md:grid-cols-2  " >
+    
+
+    {data.map((itmes)=>(
+      <div className="card w-96 bg-base-100 shadow-xl ">
+       <figure><img src={itmes.img} alt="Shoes" /></figure>
+      <div className="card-body">
+        <h2 className="card-title">{itmes.name}</h2>
+        <p>{itmes.short}</p>
+       
+      </div>
+    </div>
+    ))}
+     
+    </div>
+    </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // basePath: '/*build',
+}
 
 module.exports = nextConfig
